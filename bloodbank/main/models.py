6ac1929397a5f0d5790244c04d1814387786e213
rajeshbhojannagari <143255434+rajeshbhojannagari@users.noreply.github.com>
2026-05-13 22:56:53 +0530

    added models code in main folder

--- bloodbank/main/models.py
+++ bloodbank/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Donor(models.Model):
+    name = models.CharField(max_length=100)
+    blood_group = models.CharField(max_length=5)
+    city = models.CharField(max_length=100)
+    phone = models.CharField(max_length=15)
+    availability = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
