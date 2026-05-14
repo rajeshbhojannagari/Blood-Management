96cc4270e76413dd880e213545b59de4f61adb0f
rajeshbhojannagari <143255434+rajeshbhojannagari@users.noreply.github.com>
2026-05-14 22:52:38 +0530

    change in urls.py

--- bloodbank/main/urls.py
+++ bloodbank/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('add/', views.add_donor, name='add_donor'),
+    path('donors/', views.donors, name='donors'),
 ]
