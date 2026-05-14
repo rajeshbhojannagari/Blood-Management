83d70dfe4c5498624042e3baf56c8630635e9898
rajeshbhojannagari <143255434+rajeshbhojannagari@users.noreply.github.com>
2026-05-14 22:24:54 +0530

    change in code of db

--- bloodbank/main/models.py
+++ bloodbank/main/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 class Donor(models.Model):
+    BLOOD_GROUPS = [
+        ('A+', 'A+'),
+        ('A-', 'A-'),
+        ('B+', 'B+'),
+        ('B-', 'B-'),
+        ('O+', 'O+'),
+        ('O-', 'O-'),
+        ('AB+', 'AB+'),
+        ('AB-', 'AB-'),
+    ]
