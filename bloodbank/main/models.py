e8442f3d14301b4bf06f32a4142785a7d2cd1b63
rajeshbhojannagari <143255434+rajeshbhojannagari@users.noreply.github.com>
2026-05-15 22:07:01 +0530

    change in code

--- bloodbank/main/models.py
+++ bloodbank/main/models.py
@@ -14,7 +14,7 @@ class Donor(models.Model):
     age = models.IntegerField(default=18)
     blood_group = models.CharField(max_length=5, choices=BLOOD_GROUPS)
     city = models.CharField(max_length=100)
-    
+    phone = models.CharField(max_length=15)
     availability = models.BooleanField(default=True)
 
     def __str__(self):
